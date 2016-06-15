d84394f7e319440f4bbe5f0bc1471b6efc5f6809
krishanuSudi <krishsudi33@gmail.com>
2016-06-15 14:38:30 +0530

    Update subscriber.js

--- examples/pub-sub/subscriber/subscriber.js
+++ examples/pub-sub/subscriber/subscriber.js
@@ -1,7 +1,8 @@
 var clientId = '11f88700-f668-11e5-95f5-bc764e102b63';
 var clientAuthToken = '0bd3a1c7817a4a1da772a89fcca48523';
 //var topic = 'listener_topic';
-var topic = 'mark_topic';
+var publish_topic = 'test_topic';
+var subscribe_topic = 'mark_topic';
 
 var options = {
 	enableSsl : true
@@ -52,5 +53,27 @@ var disConnectHandler = function(){
 var oneToOneMessageHandler = function(){
 
 };
+
+var publishMessage2 = function(){
+	var msg = document.getElementById("message-box2").value;
+	instaMsg1.publish(publish_topic, msg, 0, 1, pubMsgHandler, 100);
+	
+};
+
+var pubMsgHandler = function(msg){
+	var message = msg.result();
+    console.log(message);
+}
+
+var pubMsgHandler = function(msg){
+	var message = msg.result();
+    var table = document.getElementById("recieved");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = message.destinationName;
+	cell2.innerHTML = message.payload;
+}
 
 connect();
